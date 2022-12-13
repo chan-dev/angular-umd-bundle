875c9000cf6496adc14e5c1456d1b0d6e9181925
chan-dev <christian.m.austria@gmail.com>
2022-12-13 09:11:10 +0800

    Remove unused files

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
-import { ScriptsComponent } from './scripts/scripts.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ScriptsComponent,
-  },
 ];
 
 @NgModule({
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,39 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { HighchartsChartModule } from 'highcharts-angular';
 
-import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookCollectionComponent } from './book-collection/book-collection.component';
-import { BookListComponent } from './book-list/books-list.component';
-import { ScriptsComponent } from './scripts/scripts.component';
-import { BooksEffects } from './state/books.effects';
-import { booksReducer } from './state/books.reducer';
-import { collectionReducer } from './state/collection.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ScriptsComponent,
-    BookListComponent,
-    BookCollectionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HighchartsChartModule,
     HttpClientModule,
-    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production,
-    }),
-    EffectsModule.forRoot([BooksEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
